c34739e40c86d236794cfab0b34f5654270a53f3
Sultan-html <aik709470@gmail.com>
2024-11-10 20:00:24 +0600

    исправил мелкие ошибки

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/base/',locals('apps.base.urls'))
+    path('api/v1/base/',include('apps.base.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
